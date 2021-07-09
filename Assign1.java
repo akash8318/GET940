79e615ba649b1a2e7b9ca376e73ec8ae0c111c23
akash8318 <46664009+akash8318@users.noreply.github.com>
2021-07-09 23:31:51 +0530

    Add files via upload

--- Assign1.java
+++ Assign1.java
@@ -1,1 +1,31 @@
+import java.util.*;
+public class Assign1 
+ {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+	      System.out.println("Enter the number of units");
+	      System.out.println();
+	      int n=sc.nextInt();
+	      double billpay=0;
+	      
+      	if(n<100)
+	 {
+	      billpay=n*1.20;
+	  }
+	else if(n<300)
+	 {
+	   billpay=100*1.20+(n-100)*2;
+	 }
+	else if(n>300)
+	{
+	    billpay=100*1.20+200*2+(n-300)*3;
+	}
+      	System.out.println("Amount"+billpay);
+	}
+ }
+
+
 
